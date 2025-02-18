d7003d60981db327f23af554878adc381d1f4827
Jordi Sevilla Fortuny <Jordisevilla2001@gmail.com>
2025-02-18 15:48:52 +0100

    Fix script

--- src/evaluate.R
+++ src/evaluate.R
@@ -19,7 +19,7 @@ output_file <- args[4]
 
 # thresholds
 thresholds <- list()
-for line in readLines(sprintf("%s/thresholds.tsv", data_dir)){
+for (line in readLines(sprintf("%s/thresholds.tsv", data_dir))){
     atb <- strsplit(line, "\t")[[1]][1]
     th  <- strsplit(line, "\t")[[1]][2]
     thresholds[[atb]] <- as.numeric(th)
@@ -35,7 +35,7 @@ output_table <- data.frame(
 
 # predict
 for (atb in atbs){
-    model <- sprintf("%s/%s_model.rds", data_dir, atb) %>%
+    model <- sprintf("%s/%s.rds", data_dir, atb) %>%
         readRDS()
     data <- sprintf("%s/matrix_%s.txt", temp_dir, atb) %>%
         vroom()
@@ -55,71 +55,6 @@ for (atb in atbs){
             )
     }
 }
-
-# Fosfomycin prediction
-fosfo_model <- readRDS(fosfo_model)
-fosfo_data <- vroom::vroom(fosfo_data)
-
-fosfo_prediction <- predict(
-    fosfo_model,
-    xgb.DMatrix(data = data.matrix(fosfo_data[-1]))
-    )
-
-
-
-for (x in 1:nrow(fosfo_data)){    
-
-    output_table <- add_row(
-        output_table,
-        Sample = fosfo_data$ID[x],
-        Atb = "Fosfomycin",
-        Prob_resistance = fosfo_prediction[x],
-        Prediction = ifelse(fosfo_prediction[x] > FOSFO_TH, "R", "S")
-        )
-
-}
-
-
-# Amikacin prediction
-ami_model <- readRDS(ami_model)
-ami_data <- vroom::vroom(ami_data)
-
-ami_prediction <- predict(
-    ami_model,
-    xgb.DMatrix(data = data.matrix(ami_data[-1]))
-    )
-
-for (x in 1:nrow(ami_data)){    
-
-    output_table <- add_row(
-        output_table,
-        Sample = ami_data$ID[x],
-        Atb = "Amikacin",
-        Prob_resistance = ami_prediction[x],
-        Prediction = ifelse(ami_prediction[x] > AMI_TH, "R", "S")
-        )
-}
-
-# Piperacillin prediction
-pip_model <- readRDS(pip_model)
-pip_data <- vroom::vroom(pip_data)
-
-pip_prediction <- predict(
-    pip_model,
-    xgb.DMatrix(data = data.matrix(pip_data[-1]))
-    )
-
-for (x in 1:nrow(pip_data)){    
-
-    output_table <- add_row(
-        output_table,
-        Sample = pip_data$ID[x],
-        Atb = "Piperacillin",
-        Prob_resistance = pip_prediction[x],
-        Prediction = ifelse(pip_prediction[x] > PIP_TH, "R", "S")
-        )
-}
-
 
 # Write output
 output_table %>%
